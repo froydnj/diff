4842dcde6855f0ac89937875f4be372607b44606
Moskvitin Andrey <archimag@gmail.com>
2011-04-27 13:22:35 +0400

    simplifies the creation of "diff generators" outside the library

--- package.lisp
+++ package.lisp
@@ -2,5 +2,27 @@
 
 (defpackage :diff
   (:use :cl)
-  (:export #:generate-diff #:unified-diff #:context-diff
-           #:*diff-context-lines*))
+  (:export #:*diff-context-lines*
+           #:generate-diff
+           #:unified-diff #:context-diff
+           
+           #:render-diff
+           #:render-diff-window
+           #:format-diff
+           #:format-diff-string
+
+           #:diff
+           #:original-pathname
+           #:modified-pathname
+           #:diff-window-class
+           #:diff-windows
+
+           #:diff-window
+           #:original-start-line
+           #:original-length
+           #:modified-start-line
+           #:modified-length
+           #:window-chunks
+
+           #:chunk-kind
+           #:chunk-lines))
